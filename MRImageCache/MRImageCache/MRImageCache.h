0c336f7276daf9217a6880f110266c54d2484897
Rocco Del Priore <roccodelpriore@me.com>
2016-03-16 14:21:59 -0500

    Initialized with Empty Project

---
+++ MRImageCache/MRImageCache/MRImageCache.h
@@ -0,0 +1,19 @@
+//
+//  MRImageCache.h
+//  MRImageCache
+//
+//  Created by Rocco Del Priore on 3/16/16.
+//  Copyright © 2016 Rocco Del Priore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MRImageCache.
+FOUNDATION_EXPORT double MRImageCacheVersionNumber;
+
+//! Project version string for MRImageCache.
+FOUNDATION_EXPORT const unsigned char MRImageCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MRImageCache/PublicHeader.h>
+
+
